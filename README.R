e9e7b31957bde64f5905f347e9a4a533729af5a3
Arsema Merid Mekonnen <ve0540bz@mnstate.edu>
2019-05-02 14:09:11 -0500

    Lab practice

---
+++ README.R
@@ -0,0 +1,9 @@
+
+
+## Lab Practical
+
+Mosquito counts in three counties
+
+The file of the lab practice will show you the data with mean and standard error of the three counties.
+
+here is the link to the data:
